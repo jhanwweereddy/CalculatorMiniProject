b112ee0c1339e3bcc50f5742e0035a1fff964104
jhanwweereddy <Shree.Jhanwwee@iiitb.org>
2022-04-18 15:04:35 +0530

    updated tests1!

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,56 +9,56 @@ public class CalculatorTest {
     @Test
     public void squarerootTruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 3, calc.SquareRoot(9), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 6, calc.SquareRoot(36), DELTA);
+        assertEquals ("Square Root (True Positive)", 3, calc.SquareRoot(9), DELTA);
+        assertEquals ("Square Root (True Positive)", 6, calc.SquareRoot(36), DELTA);
     }
 
     @Test
     public void squarerootFalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 4, calc.SquareRoot(25), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 10, calc.SquareRoot(121), DELTA);
+        assertNotEquals ("Square Root (False Positive)", 4, calc.SquareRoot(25), DELTA);
+        assertNotEquals ("Square Root (False Positive)", 10, calc.SquareRoot(121), DELTA);
     }
 
     @Test
     public void factorialTruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 720, calc.factorial(6), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 24, calc.factorial(4), DELTA);
+        assertEquals ("Factorial function (True Positive)", 720, calc.factorial(6), DELTA);
+        assertEquals ("Factorial function (True Positive)", 24, calc.factorial(4), DELTA);
     }
 
     @Test
     public void factorialFalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 0, calc.factorial(0), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 9, calc.factorial(3), DELTA);
+        assertNotEquals ("Factorial function (False Positive)", 0, calc.factorial(0), DELTA);
+        assertNotEquals ("Factorial function (False Positive)", 9, calc.factorial(3), DELTA);
     }
 
     @Test
     public void logTruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 0, calc.log(1), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 2.302585092994046, calc.log(10), DELTA);
+        assertEquals ("Natural Logarithm (True Positive)", 0, calc.log(1), DELTA);
+        assertEquals ("Natural Logarithm (True Positive)", 2.302585092994046, calc.log(10), DELTA);
     }
 
     @Test
     public void logFalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 0, calc.log(4), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 1, calc.log(5), DELTA);
+        assertNotEquals ("Natural Logarithm (False Positive)", 0, calc.log(4), DELTA);
+        assertNotEquals ("Natural Logarithm (False Positive)", 1, calc.log(5), DELTA);
     }
 
     @Test
     public void powerTruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 1, calc.power(1, 1), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 64, calc.power(4, 3), DELTA);
+        assertEquals ("Power function (True Positive)", 1, calc.power(1, 1), DELTA);
+        assertEquals ("Power function (True Positive)", 64, calc.power(4, 3), DELTA);
     }
 
     @Test
     public void powerFalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 1, calc.power(2, 1), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 12, calc.power(2, 6), DELTA);
+        assertNotEquals ("Power function (False Positive)", 1, calc.power(2, 1), DELTA);
+        assertNotEquals ("Power function (False Positive)", 12, calc.power(2, 6), DELTA);
     }
 }
